ca3a8e938911d86f0954627bf176a6c8ad17e3b0
CMIke <99619881+JuanMiguelSanchezAguilar@users.noreply.github.com>
2023-03-22 14:52:26 -0600

    Conexion a bd
    Se realizo la conexion a la bd

---
+++ Proyecto Punto De Venta/Modelo/conexionMysql.cs
@@ -0,0 +1,41 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Proyecto_Punto_De_Venta.Modelo
+{
+    internal class conexionMysql : conexion
+    {
+        private MySqlConnection connection;
+        private string cadenaConexion;
+        public conexionMysql()
+        {
+            cadenaConexion = "Database=" + database +
+                "; DataSource=" + server +
+                "; User Id= " + user +
+                "; Password=" + password;
+
+            connection = new MySqlConnection(cadenaConexion);
+        }
+        public MySqlConnection GetConnection()
+        {
+            try
+            {
+                if (connection.State != System.Data.ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                throw;
+            }
+            return connection;
+        }
+    }
+}
